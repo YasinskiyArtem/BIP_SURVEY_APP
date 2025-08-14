5f3f86e793777eb7a2189bb522ba026499d7608e
YASINSKIY ARTEM <101966909+YasinskiyArtem@users.noreply.github.com>
2025-08-14 10:57:41 +0300

    sign_but

--- frontend/src/components/SigninButton.tsx
+++ frontend/src/components/SigninButton.tsx
@@ -46,7 +46,7 @@ const SignInButton: React.FC<SignInButtonProps> = ({ text }) => {
     e.preventDefault();
     try {
       //const response = await fetch('http://localhost:8000/api/auth/login', {
-      const response = await fetch('/api/auth/login', {
+      const response = await fetch('/api/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -107,7 +107,7 @@ const SignInButton: React.FC<SignInButtonProps> = ({ text }) => {
     if (token && otpToken) {
       try {
         //const response = await fetch('http://localhost:8000/api/auth/otp/verify', {
-        const response = await fetch('/api/auth/otp/verify', {
+        const response = await fetch('/api/api/auth/otp/verify', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
