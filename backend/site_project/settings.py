8964ddd19e59bc7eb2bacb0c94a0d95db74dc5a9
YASINSKIY ARTEM <101966909+YasinskiyArtem@users.noreply.github.com>
2025-08-24 21:19:03 +0300

    adminpanel

--- backend/site_project/settings.py
+++ backend/site_project/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bp^x^r6!8kogkbf^#w)qkqqb#*xw(34we#wz)&lnnv-a1*qidp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['survey-form.ru', 'www.survey-form.ru', 'localhost', '127.0.0.1', '195.133.201.21']
 
-
+CSRF_TRUSTED_ORIGINS=["https://survey-form.ru"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -161,9 +161,10 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 #STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Важно!
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Важно!
+#STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
